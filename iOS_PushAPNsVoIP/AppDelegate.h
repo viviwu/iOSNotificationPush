293fd735eb469de266b4865fc186c69b88a901c6
viviwu <vivi705@qq.com>
2017-09-29 15:38:29 +0800

    Initial Commit

---
+++ iOS_PushAPNsVoIP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_PushAPNsVoIP
+//
+//  Created by Qway on 2017/9/29.
+//  Copyright © 2017年 viviwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_PushAPNsVoIP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_PushAPNsVoIP
+//
+//  Created by Qway on 2017/9/29.
+//  Copyright © 2017年 viviwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
